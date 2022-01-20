c35e5fbf721ce138b0e73eddd3dae58fc3daa3fd
puzhixu <hauming711cyk@gmail.com>
2022-01-20 17:07:16 +0800

    Update Controller

--- Assets/Scripts/Controller/PlayerMovementController.cs
+++ Assets/Scripts/Controller/PlayerMovementController.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    public float gravity = 20.0f;
+
+    private Vector2 m_beginPos;
+    private Vector2 m_dragPos;
+
+    private float m_speed;
+
+    private Vector3 m_moveDirection;
+
+    private float m_rotateAngle;
+
+    private CharacterController m_charCtrllor;
+
+    [SerializeField]
+    private float m_speedOffset = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_charCtrllor = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch currentTouch = Input.touches[0];
+
+            if(currentTouch.phase == TouchPhase.Began)
+            {
+                m_beginPos = currentTouch.position;
+            }
+
+            if (currentTouch.phase == TouchPhase.Moved)
+            {
+                m_dragPos = currentTouch.position;
+
+                m_speed = (m_dragPos - m_beginPos).magnitude;
+
+                m_rotateAngle = Mathf.Atan2((m_dragPos.y - m_beginPos.y),
+                    (m_dragPos.x - m_beginPos.x)) * Mathf.Rad2Deg - 90f;
+
+                if (m_rotateAngle <= 100)
+                    m_rotateAngle -= 360;
+
+                transform.eulerAngles = new Vector3(0f, m_rotateAngle * -1, 0f);
+            }
+
+            if (currentTouch.phase == TouchPhase.Ended)
+            {
+                m_rotateAngle = 0f;
+                m_speed = 0f;
+            }
+
+            if (m_charCtrllor.isGrounded)
+            {
+                m_moveDirection = transform.forward * Time.deltaTime * m_speed * m_speedOffset;
+            }
+
+            m_moveDirection.y -= gravity * Time.deltaTime;
+
+            m_charCtrllor.Move(m_moveDirection);
+
+            this.GetComponent<Animator>().SetInteger("AnimationPar", m_speed > 5f ? 1 : 0);
+        }
     }
 }
